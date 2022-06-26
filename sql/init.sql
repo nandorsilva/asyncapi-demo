4399563bdcf54e9995c2174247fe60b60ba46592
Fernando <fernandos007@gmail.com>
2022-06-26 11:28:31 -0300

    Commit para artigo cloudevents

--- sql/init.sql
+++ sql/init.sql
@@ -10,9 +10,6 @@ CREATE TABLE produtos (
   nome VARCHAR(255) NOT NULL,
   descricao VARCHAR(512)
 );
-INSERT INTO produtos(nome,descricao)  VALUES ('Lapis','lapis de escrever');
-INSERT INTO produtos(nome,descricao)  VALUES ('Borracha','borracha muito boa');
-INSERT INTO produtos(nome,descricao)  VALUES ('Celular','celular hawaui');
-INSERT INTO produtos(nome,descricao)  VALUES ('TV','50 polegadas');
+
 EXEC sys.sp_cdc_enable_table @source_schema = 'dbo', @source_name = 'produtos', @role_name = NULL, @supports_net_changes = 0;
 GO
---
+++ sql/insert.sql
@@ -0,0 +1,7 @@
+USE dbEcommerce;
+Go
+INSERT INTO produtos(nome,descricao)  VALUES ('Lapis','lapis de escrever');
+INSERT INTO produtos(nome,descricao)  VALUES ('Borracha','borracha muito boa');
+INSERT INTO produtos(nome,descricao)  VALUES ('Celular','celular hawaui');
+INSERT INTO produtos(nome,descricao)  VALUES ('TV','50 polegadas');
+GO
